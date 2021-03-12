a286cfa24c67276d802b93d27fb1f105ae8ed671
Mickaël Menu <mickael.menu@gmail.com>
2021-03-12 11:37:38 +0100

    Add HttpClient and HttpRequest types

---
+++ r2-shared/src/main/java/org/readium/r2/shared/util/http/HttpClient.kt
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2021 Readium Foundation. All rights reserved.
+ * Use of this source code is governed by the BSD-style license
+ * available in the top-level LICENSE file of the project.
+ */
+
+package org.readium.r2.shared.util.http
+
+import org.readium.r2.shared.util.mediatype.MediaType
+import java.util.*
+
+/**
+ * An HTTP client performs HTTP requests.
+ *
+ * You may provide a custom implementation, or use the [DefaultHttpClient] one which relies on
+ * native APIs.
+ */
+interface HttpClient {
+
+    /**
+     * Fetches the resource from the given [request].
+     */
+    suspend fun fetch(request: HttpRequest): HttpTry<HttpFetchResponse>
+
+    /**
+     * Downloads a resource progressively.
+     *
+     * Useful in the context of streaming media playback.
+     *
+     * @param request Request to the downloaded resource.
+     * @param range If provided, issue a byte range request.
+     * @param receiveResponse Callback called when receiving the initial response, before consuming
+     *        its body. You can also access it in the completion block after consuming the data.
+     * @param consumeData Callback called for each chunk of data received. Callers are responsible
+     *        to accumulate the data if needed.
+     */
+    suspend fun progressiveDownload(
+        request: HttpRequest,
+        range: LongRange? = null,
+        receiveResponse: ((HttpResponse) -> Void)? = null,
+        consumeData: (chunk: ByteArray, progress: Double?) -> Unit,
+    ): HttpTry<HttpResponse>
+
+}
+
+data class HttpFetchResponse(
+    val response: HttpResponse,
+    val body: ByteArray,
+)
+
+/**
+ * Represents a successful HTTP response received from a server.
+ *
+ * @param headers HTTP response headers, indexed by their name.
+ * @param mediaType Media type sniffed from the `Content-Type` header and response body. Falls back
+ *        on `application/octet-stream`.
+ */
+data class HttpResponse(
+    val headers: Map<String, String>,
+    val mediaType: MediaType,
+) {
+
+    /**
+     * Finds the value of the first header matching the given name.
+     * In keeping with the HTTP RFC, HTTP header field names are case-insensitive.
+     */
+    fun valueForHeader(name: String): String? {
+        @Suppress("NAME_SHADOWING")
+        val name = name.toLowerCase(Locale.ROOT)
+        for ((n, v) in headers) {
+            if (name == n.toLowerCase(Locale.ROOT)) {
+                return v
+            }
+        }
+        return null
+    }
+
+    /**
+     * Indicates whether this server supports byte range requests.
+     */
+    val acceptsByteRanges: Boolean get() {
+        return valueForHeader("Accept-Ranges")?.toLowerCase(Locale.ROOT) == "bytes"
+            || valueForHeader("Content-Range")?.toLowerCase(Locale.ROOT)?.startsWith("bytes") == true
+    }
+
+    /**
+     * The expected content length for this response, when known.
+     *
+     * Warning: For byte range requests, this will be the length of the chunk, not the full
+     * resource.
+     */
+    val contentLength: Long? get() =
+        valueForHeader("Content-Length")
+            ?.toLongOrNull()
+            ?.takeIf { it >= 0 }
+
+}
---
+++ r2-shared/src/main/java/org/readium/r2/shared/util/http/HttpRequest.kt
@@ -0,0 +1,62 @@
+package org.readium.r2.shared.util.http
+
+import kotlin.time.Duration
+import kotlin.time.ExperimentalTime
+
+/**
+ * Holds the information about an HTTP request performed by an [HttpClient].
+ * @param url Address of the remote resource to request.
+ * @param method HTTP method to use for the request.
+ * @param headers Additional HTTP headers to use.
+ * @param connectTimeout Timeout used when establishing a connection to the resource. A null timeout
+ *        is interpreted as the default value, while a timeout of zero as an infinite timeout.
+ * @param connectTimeout Timeout used when reading the input stream. A null timeout is interpreted
+ *        as the default value, while a timeout of zero as an infinite timeout.
+ * @param allowUserInteraction If true, the user might be presented with interactive dialogs, such
+ *        as popping up an authentication dialog.
+ */
+@OptIn(ExperimentalTime::class)
+data class HttpRequest(
+    val url: String,
+    val method: Method = Method.GET,
+    val headers: Map<String, String> = mapOf(),
+    val connectTimeout: Duration? = null,
+    val readTimeout: Duration? = null,
+    val allowUserInteraction: Boolean = false,
+) {
+
+    /** Supported HTTP methods. */
+    enum class Method {
+        GET, HEAD, POST, PUT;
+    }
+
+    companion object {
+        operator fun invoke(build: Builder.() -> Unit): HttpRequest =
+            Builder().apply(build).build()
+    }
+
+    data class Builder(
+        var url: String = "",
+        var method: Method = Method.GET,
+        var headers: MutableMap<String, String> = mutableMapOf(),
+        var connectTimeout: Duration? = null,
+        var readTimeout: Duration? = null,
+        var allowUserInteraction: Boolean = false,
+    ) {
+
+        fun setHeader(key: String, value: String) {
+            headers[key] = value
+        }
+
+        fun build(): HttpRequest = HttpRequest(
+            url = url,
+            method = method,
+            headers = headers.toMap(),
+            connectTimeout = connectTimeout,
+            readTimeout = readTimeout,
+            allowUserInteraction = allowUserInteraction,
+        )
+
+    }
+
+}
