f079700a8bc8cef20244f5c99416f601f6a71346
Mickaël Menu <mickael.menu@gmail.com>
2021-05-06 15:29:12 +0200

    Implement search options for the IcuSearchService

--- r2-shared/src/main/java/org/readium/r2/shared/publication/services/search/IcuSearchService.kt
+++ r2-shared/src/main/java/org/readium/r2/shared/publication/services/search/IcuSearchService.kt
@@ -6,15 +6,18 @@
 
 package org.readium.r2.shared.publication.services.search
 
+import android.icu.text.BreakIterator
 import android.icu.text.Collator
 import android.icu.text.RuleBasedCollator
 import android.icu.text.StringSearch
 import android.os.Build
+import androidx.annotation.RequiresApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.readium.r2.shared.fetcher.DefaultResourceContentExtractorFactory
 import org.readium.r2.shared.fetcher.ResourceContentExtractor
 import org.readium.r2.shared.publication.*
+import org.readium.r2.shared.publication.services.search.SearchService.Option
 import org.readium.r2.shared.util.Ref
 import org.readium.r2.shared.util.Try
 import timber.log.Timber
@@ -31,15 +34,17 @@ import java.util.*
  */
 class IcuSearchService private constructor(
     private val publication: Ref<Publication>,
-    private val surroundingContextLength: Int,
+    private val snippetLength: Int,
     private val extractorFactory: ResourceContentExtractor.Factory,
 ) : SearchService {
 
-    override val capabilities: Set<SearchService.Capability> get() = emptySet()
-    override val supportedOptions: Set<SearchService.Option> get() = emptySet()
-    override val defaultOptions: Set<SearchService.Option> get() = emptySet()
+    override val options: Set<Option> get() = setOf(
+        Option.CaseSensitive(false),
+        Option.DiacriticSensitive(false),
+        Option.WholeWord(false),
+    )
 
-    override suspend fun search(query: String, options: Set<SearchService.Option>): SearchTry<SearchIterator> =
+    override suspend fun search(query: String, options: Set<Option>): SearchTry<SearchIterator> =
         try {
             val publication = publication() ?: throw IllegalStateException("No Publication object")
             val locale = publication.metadata.locale ?: Locale.getDefault()
@@ -49,7 +54,7 @@ class IcuSearchService private constructor(
             Try.failure(SearchException.wrap(e))
         }
 
-    private inner class Iterator(val locale: Locale, val query: String, val options: Set<SearchService.Option>) : SearchIterator {
+    private inner class Iterator(val locale: Locale, val query: String, val options: Set<Option>) : SearchIterator {
         /**
          * Index of the last reading order resource searched in.
          */
@@ -96,14 +101,7 @@ class IcuSearchService private constructor(
             val locators = mutableListOf<Locator>()
 
             withContext(Dispatchers.IO) {
-                val collator = Collator.getInstance(locale) as RuleBasedCollator
-                // http://userguide.icu-project.org/collation/customization
-                // PRIMARY = ignore accents and case
-                // PRIMARY + caseLevel true = ignore accents
-                // SECONDARY = ignore case
-                collator.strength = Collator.SECONDARY
-//            collator.isCaseLevel = true
-                val iter = StringSearch(query, StringCharacterIterator(text), collator)
+                val iter = createStringSearch(text)
                 var start = iter.first()
                 while (start != android.icu.text.SearchIterator.DONE) {
                     val end = start + iter.matchLength
@@ -115,8 +113,8 @@ class IcuSearchService private constructor(
                         ),
                         text = Locator.Text(
                             highlight = text.substring(start until end),
-                            before = text.substring((start - surroundingContextLength).coerceAtLeast(0) until start),
-                            after = text.substring(end until (end + surroundingContextLength).coerceAtMost(text.length)),
+                            before = text.substring((start - snippetLength).coerceAtLeast(0) until start),
+                            after = text.substring(end until (end + snippetLength).coerceAtMost(text.length)),
                         )
                     )
                     locators.add(locator)
@@ -127,13 +125,47 @@ class IcuSearchService private constructor(
 
             return locators
         }
-   }
+
+        @RequiresApi(Build.VERSION_CODES.N)
+        private fun createStringSearch(text: String): StringSearch {
+            val caseSensitive = options.get<Option.CaseSensitive>()?.on ?: false
+            var diacriticSensitive = options.get<Option.DiacriticSensitive>()?.on ?: false
+            val wholeWord = options.get<Option.WholeWord>()?.on ?: false
+
+            // Because of an issue (see FIXME below), we can't have case sensitivity without also
+            // enabling diacritic sensitivity.
+            diacriticSensitive = diacriticSensitive || caseSensitive
+
+            // http://userguide.icu-project.org/collation/customization
+            // ignore diacritics and case = primary strength
+            // ignore diacritics = primary strength + caseLevel on
+            // ignore case = secondary strength
+            val collator = Collator.getInstance(locale) as RuleBasedCollator
+            if (!diacriticSensitive) {
+                collator.strength = Collator.PRIMARY
+                if (caseSensitive) {
+                    // FIXME: This doesn't seem to work despite the documentation indicating:
+                    // > To ignore accents but take cases into account, set strength to primary and case level to on.
+                    // > http://userguide.icu-project.org/collation/customization
+                    collator.isCaseLevel = true
+                }
+            } else if (!caseSensitive) {
+                collator.strength = Collator.SECONDARY
+            }
+
+            val breakIterator: BreakIterator? =
+                if (wholeWord) BreakIterator.getWordInstance()
+                else null
+
+            return StringSearch(query, StringCharacterIterator(text), collator, breakIterator)
+        }
+    }
 
     companion object {
         fun createFactory(
-            surroundingContextLength: Int = 70,
+            snippetLength: Int = 200,
             extractorFactory: ResourceContentExtractor.Factory = DefaultResourceContentExtractorFactory()
         ): (Publication.Service.Context) -> IcuSearchService =
-            { context -> IcuSearchService(context.publication, surroundingContextLength, extractorFactory) }
+            { context -> IcuSearchService(context.publication, snippetLength, extractorFactory) }
     }
 }
--- r2-shared/src/main/java/org/readium/r2/shared/publication/services/search/SearchService.kt
+++ r2-shared/src/main/java/org/readium/r2/shared/publication/services/search/SearchService.kt
@@ -17,8 +17,9 @@ import org.readium.r2.shared.publication.LocatorCollection
 import org.readium.r2.shared.publication.Publication
 import org.readium.r2.shared.publication.ServiceFactory
 import org.readium.r2.shared.util.Try
-import org.readium.r2.shared.util.flatMap
 import org.readium.r2.shared.util.http.HttpException
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 typealias SearchTry<SuccessT> = Try<SuccessT, SearchException>
 
@@ -84,37 +85,60 @@ sealed class SearchException(content: Content, cause: Throwable? = null) : UserE
 interface SearchService : Publication.Service {
 
     /**
-     * Represents a feature supported by the Search Service.
-     * For example, whether the search supports boolean operators.
+     * Represents an option and its current value supported by a service.
+     *
+     * Implementation note: key and rawValue are opened to be able to use @Parcelize in Custom case.
      */
-    sealed class Capability(val uri: String) : Parcelable {
-        @Parcelize object CaseInsensitive : Capability("case-insensitive")
-        @Parcelize class MinimumQueryLength(val int: Int) : Capability("min-query-length")
-    }
+    sealed class Option(open val key: String, protected open val rawValue: Any?) : Parcelable {
 
-    sealed class Option(val uri: String) : Parcelable {
-        @Parcelize object CaseInsensitive : Option("case-insensitive")
-        @Parcelize object WholeWord : Option("whole-word")
-    }
+        val value: String? get() =
+            rawValue?.toString()?.takeIf { it.isNotBlank() }
 
-    /**
-     * Search capabilities supported by this service.
-     */
-    val capabilities: Set<Capability>
+        /**
+         * Whether the search will differentiate between capital and lower-case letters.
+         */
+        @Parcelize class CaseSensitive(val on: Boolean) : Option("case-sensitive", on)
+
+        /**
+         * Whether the search will differentiate between letters with accents or not.
+         */
+        @Parcelize class DiacriticSensitive(val on: Boolean) : Option("diacritic-sensitive", on)
+
+        /**
+         * Whether the query terms will match full words and not parts of a word.
+         */
+        @Parcelize class WholeWord(val on: Boolean) : Option("whole-word", on)
+
+        /**
+         * Matches results similar but not identical to the query, such as reordered or words with a
+         * related stem. For example, "banana split" would match "I love banana split" but also
+         * "splitting all the bananas". When close variants are enabled, surround terms with double
+         * quotes for an exact match.
+         */
+        @Parcelize class CloseVariants(val on: Boolean) : Option("close-variants", on)
+
+        /**
+         * Matches results with typos or similar spelling.
+         *
+         * See https://en.wikipedia.org/wiki/Approximate_string_matching
+         */
+        @Parcelize class Fuzzy(val on: Boolean) : Option("fuzzy", on)
+
+        /**
+         * A custom option implemented by a Search Service which is not officially recognized by
+         * Readium.
+         */
+        @Parcelize class Custom(override val key: String, override val rawValue: String) : Option(key, rawValue)
+    }
 
     /**
      * All search options available for this service.
      *
-     * Used to display the user options in the search interface.
+     * Also holds the default value for these options, which can be useful to setup the views
+     * in the search interface. If an option is missing when calling search(), its value is assumed
+     * to be the default one.
      */
-    val supportedOptions: Set<Option>
-
-    /**
-     * Default search options enabled with this service.
-     *
-     * Used to pre-check options in the search interface.
-     */
-    val defaultOptions: Set<Option>
+    val options: Set<Option>
 
     /**
      * Starts a new search through the publication content, with the given [query].
@@ -186,3 +210,6 @@ interface SearchIterator {
         }
     }
 }
+
+inline fun <reified T: SearchService.Option> Set<SearchService.Option>.get(): T? =
+    firstOrNull { it is T } as? T
