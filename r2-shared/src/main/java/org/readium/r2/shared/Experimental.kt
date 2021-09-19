e8064a475ca06a72415b158865cce6102121d768
Mickaël Menu <mickael.menu@gmail.com>
2021-09-19 11:54:37 +0200

    Remove AudiobookNavigator annotation

--- r2-shared/src/main/java/org/readium/r2/shared/Experimental.kt
+++ r2-shared/src/main/java/org/readium/r2/shared/Experimental.kt
@@ -18,8 +18,3 @@ annotation class PdfSupport
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.TYPEALIAS, AnnotationTarget.PROPERTY)
 annotation class Search
-
-@RequiresOptIn(message = "The new Audiobook navigator is still experimental. The API may be changed in the future without notice.")
-@Retention(AnnotationRetention.BINARY)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.TYPEALIAS, AnnotationTarget.PROPERTY)
-annotation class AudiobookNavigator
